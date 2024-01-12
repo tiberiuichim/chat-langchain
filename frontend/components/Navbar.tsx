83fb4301e069a4a983406933c59ea15ed94c985c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-12 08:46:11 +0200

    WIP

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,17 +1,18 @@
+"use client";
 import * as React from "react";
 
-import Link from "next/link";
+// import Link from "next/link";
 // import { Icons } from "@/components/icons";
 
 export function Navbar() {
   return (
     <div className="flex">
-      <Link href="/">
+      <button onClick={() => window.location.reload()}>
         <img
           style={{ width: "100px" }}
           src="https://www.eea.europa.eu/en/newsroom/branding-materials/eea_logo_compact_en-1.svg/@@images/image-1-ec9ee8fd8059f5a2418196bfdee570e2.svg"
         />
-      </Link>
+      </button>
       <small>Beta</small>
     </div>
   );
