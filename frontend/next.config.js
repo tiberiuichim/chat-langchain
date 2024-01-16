2dedf0205ba0da177ff0324acf62a36464e8264f
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-16 10:08:56 +0200

    Dockerfile for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,7 @@ const config = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  output: "standalone",
 };
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
