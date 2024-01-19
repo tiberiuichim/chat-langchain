793a1ef4b16382938f5efa39e0b3db76f7953065
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-01-19 17:52:39 +0200

    wip

--- frontend/app/api/dbexplore/route.ts
+++ frontend/app/api/dbexplore/route.ts
@@ -1,32 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+import weaviate from "weaviate-ts-client";
 
 export const runtime = "nodejs";
 
-export async function POST(
-  req: NextRequest,
-  { params }: { params: { slug: string | string[] } },
-) {
-  let { slug } = params;
-  if (Array.isArray(slug)) {
-    slug = slug.join("/");
-  }
+export async function POST(req: NextRequest) {
+  const server = process.env.WEAVIATE_URL || "http://localhost:8060";
+  // const index = process.env.WEAVIATE_DOCS_INDEX_NAME || "";
 
-  const server = process.env.WEAVIATE_URL;
-  const index = process.env.WEAVIATE_DOCS_INDEX_NAME;
+  const url = new URL(server);
+  const options = {
+    scheme: url.protocol.replaceAll("://", "").replaceAll(":", ""),
+    host: `${url.hostname}:${url.port}`, // Replace with your endpoint
+  };
+  console.log("options", options);
 
-  // const backend = `${}/${slug}`;
-  // console.log("backend", backend);
-  // const response = await fetch(backend, req);
+  const client = weaviate.client(options);
 
-  if (!response.ok) {
-    return NextResponse.json(
-      { error: "An error occurred in backend communication" },
-      { status: response.status },
-    );
-  }
-  console.log("headers", response.headers);
+  // const allClassDefinitions = await client.schema.get();
 
-  return new Response(response.body, {
-    headers: response.headers,
-  });
+  // console.log(JSON.stringify(allClassDefinitions, null, 2));
+
+  const response = await client.schema.getter().do();
+  // console.log("response", response);
+
+  // if (!response.ok) {
+  //   return NextResponse.json(
+  //     { error: "An error occurred in backend communication" },
+  //     { status: response.status },
+  //   );
+  // }
+  // console.log("headers", response.headers);
+
+  return NextResponse.json({ indexes: response });
+
+  // (response.body, {
+  //   headers: response.headers,
+  // });
 }
--- frontend/app/dbexplorer/page.tsx
+++ frontend/app/dbexplorer/page.tsx
@@ -1,3 +1,69 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+import { PanelTopClose } from "lucide-react";
+
 export default function DBExplorerPage() {
-  return <div>Explore db</div>;
+  const [records, setRecords] = useState([]);
+  const [indexes, setIndexes] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  useEffect(() => {
+    const handler = async () => {
+      const resp = await fetch("/api/dbexplore", { method: "post" });
+      const json = await resp.json();
+      console.log("resp", json);
+      const indexes = json.indexes.classes.map((v) => v.class) || [];
+      setIndexes(indexes);
+    };
+
+    handler();
+
+    return () => {
+      //
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Explore db</h3>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            type="submit"
+            aria-label="Send"
+            onKeyDown={(e) => {
+              e.preventDefault();
+              // sendMessage();
+            }}
+            className="rounded-md border flex space-x-2"
+          >
+            <div>{activeIndex ? activeIndex : "Select an index"}</div>
+            <PanelTopClose className="text-slate-600" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {indexes.map((name) => (
+            <DropdownMenuItem key={name}>
+              <div
+                className="text-sm text-slate-500"
+                onClick={() => setActiveIndex(name)}
+              >
+                {name}
+              </div>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
