52dfcd508cc93661432c4de41f84f07d2c983509
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-18 10:35:36 +0200

    WI

--- ingest.py
+++ ingest.py
@@ -1,62 +1,71 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
 
-from typing import Literal
 import logging
 import os
+
 import weaviate
 from langchain.indexes import SQLRecordManager
 from langchain.vectorstores.weaviate import Weaviate
+from langchain_experimental.text_splitter import SemanticChunker
 
-from _index import index, Cleanup
-from utils import (
-    load_documents,
-    split_documents,
-    split_documents_tiktoken
-)
-
+from _index import Cleanup, index
 from chain import get_embeddings_model
 from constants import (
-    WEAVIATE_DOCS_INDEX_NAME,
-    WEAVIATE_API_KEY,
-    WEAVIATE_URL,
-    RECORD_MANAGER_DB_URL,
     DOCUMENTS_DIR,
+    RECORD_MANAGER_DB_URL,
+    WEAVIATE_API_KEY,
+    WEAVIATE_DOCS_INDEX_NAME,
+    WEAVIATE_URL,
 )
+from utils import load_documents, split_documents, split_documents_tiktoken
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# chunk_size: int = 400
+# chunk_overlap: int = 0
+# file_store = LocalFileStore(LOCAL_FILE_STORE)
+# child_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+#     chunk_size=chunk_size, chunk_overlap=chunk_overlap
+# )
+#
+# retriever = ParentDocumentRetriever(
+#     vectorstore=vectorstore,
+#     docstore=store,
+#     child_splitter=child_splitter,
+# )
+# retriever.add_documents(docs, ids=None)
+
+
+def derive_documents(doc, parent_splitter, child_splitter):
+    pass
+
 
 def ingest_docs(documents, cleanup: Cleanup = "full"):
-    logger.info(f"Loaded {len(documents)} from {DOCUMENTS_DIR}")
-    docs_transformed = split_documents(documents, tokenizer=None)
-#    docs_transformed = split_documents_tiktoken(documents)
-
-    # We try to return 'source' and 'title' metadata when querying vector store and
-    # Weaviate will error at query time if one of the attributes is missing from a
-    # retrieved document.
-    fallback_total_pages = len(docs_transformed)
-    for idx, doc in enumerate(docs_transformed):
-        # __import__("pdb").set_trace()
+    for idx, doc in enumerate(documents):
         if "source" not in doc.metadata:
             doc.metadata["source"] = ""
         if "title" not in doc.metadata:
             doc.metadata["title"] = doc.metadata["source"]
         if "file_path" not in doc.metadata:
             doc.metadata["file_path"] = doc.metadata["source"]
-        if "page" not in doc.metadata:
-            doc.metadata["page"] = idx + 1
 
-        title = doc.metadata["title"]
-        page = doc.metadata.get("page", 0)
-        total_pages = doc.metadata.get("total_pages", fallback_total_pages)
-        doc.metadata["title"] = f"{title} - page {page}/{total_pages}"
+    embedding = get_embeddings_model()
+
+    parent_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    child_splitter = SemanticChunker(embedding)
+    # RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    #     chunk_size=100, chunk_overlap=0
+    # )
+
+    documents = derive_documents(documents, parent_splitter)
 
     client = weaviate.Client(
         url=WEAVIATE_URL,
         auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY),
     )
-    embedding = get_embeddings_model()
     vectorstore = Weaviate(
         client=client,
         index_name=WEAVIATE_DOCS_INDEX_NAME,
@@ -70,14 +79,15 @@ def ingest_docs(documents, cleanup: Cleanup = "full"):
         f"weaviate/{WEAVIATE_DOCS_INDEX_NAME}", db_url=RECORD_MANAGER_DB_URL
     )
     record_manager.create_schema()
+    force_update = (os.environ.get("FORCE_UPDATE") or "false").lower() == "true"
 
     indexing_stats = index(
-        docs_transformed,
+        documents,
         record_manager,
         vectorstore,
         cleanup=cleanup,
         source_id_key="source",
-        force_update=(os.environ.get("FORCE_UPDATE") or "false").lower() == "true",
+        force_update=force_update,
     )
 
     logger.info(f"Indexing stats: {indexing_stats}")
@@ -89,4 +99,20 @@ def ingest_docs(documents, cleanup: Cleanup = "full"):
 
 if __name__ == "__main__":
     documents = load_documents(DOCUMENTS_DIR)
+    logger.info(f"Loaded {len(documents)} from {DOCUMENTS_DIR}")
     ingest_docs(documents)
+
+# docs_transformed = split_documents(documents, tokenizer=None)
+#    docs_transformed = split_documents_tiktoken(documents)
+
+# We try to return 'source' and 'title' metadata when querying vector store and
+# Weaviate will error at query time if one of the attributes is missing from a
+# retrieved document.
+# fallback_total_pages = len(docs_transformed)
+
+# if "page" not in doc.metadata:
+#     doc.metadata["page"] = idx + 1
+# title = doc.metadata["title"]
+# page = doc.metadata.get("page", 0)
+# total_pages = doc.metadata.get("total_pages", fallback_total_pages)
+# doc.metadata["title"] = f"{title} - page {page}/{total_pages}"
